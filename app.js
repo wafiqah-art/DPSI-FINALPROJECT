bc1737d036e2b1dcd3b31233b3a3602685e7a6a5
wafiqah-art <qahwafi77@gmail.com>
2024-07-21 15:39:09 +0700

    Update app.js

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ var Transaksi = require('./models/transaksi');
 var app = express();
 
 // view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade')
 
 app.use(logger('dev'));
 app.use(express.json());
